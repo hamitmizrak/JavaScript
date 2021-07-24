3534204ef95a667f27bcdf478755fd0c1b7ab810
HamitMizrak <hamitmizrak@gmail.com>
2021-07-24 16:26:13 +0300

    var,let,const

--- WebContent/js/template.js
+++ WebContent/js/template.js
@@ -1,12 +1,49 @@
 //comment= Yorumlama
 //tek satırlım yorumlama 
 
+//kısaltma: Ctrl+K
+
 /* 
 çoklu satırlar
 */
 
 //output
-console.log("yazılımcının kodlamada işlem yapabilmesini sağlayan yapıdır")
-window.alert("window alert")
-alert("Ekrandan Göster");
-document.write("document write 44")
+// console.log("yazılımcının kodlamada işlem yapabilmesini sağlayan yapıdır")
+// window.alert("window alert")
+// alert("Ekrandan Göster");
+// document.write("document write 44")
+
+//js yazmıyoruz: <script></script>
+
+// dinamik için değişkenler
+// değişkenlerde white space= boşluk yazalım
+//var = global değişkenlerde kullanabiliyoruz ancak tehlikelidir.
+//global değişkenler
+var kalem = "Adel 44";
+document.write(kalem);
+kalem="Rotring";
+document.write(" "+kalem);
+
+//let-const: ortak yönleri localdir
+//let-const: farklı yönleri const: değiştiremezsiniz.
+//local değişkenler
+let sayi =44;
+document.write(sayi);
+sayi=999;
+
+
+//local değişkenler const: sabittir değiştirmezsin
+const sabit=55;
+alert(sabit);
+
+
+
+
+
+
+
+
+
+
+//common rules : Genel Kurallar
+
