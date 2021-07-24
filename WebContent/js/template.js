e172335fd1598c5a188bfc7b414ebf5b69de24db
HamitMizrak <hamitmizrak@gmail.com>
2021-07-24 16:10:48 +0300

    template

---
+++ WebContent/js/template.js
@@ -0,0 +1,12 @@
+//comment= Yorumlama
+//tek satırlım yorumlama 
+
+/* 
+çoklu satırlar
+*/
+
+//output
+console.log("yazılımcının kodlamada işlem yapabilmesini sağlayan yapıdır")
+window.alert("window alert")
+alert("Ekrandan Göster");
+document.write("document write 44")
